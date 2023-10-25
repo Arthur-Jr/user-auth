5d79a4155674220bb1c40fa9d810e487d75388a8
Arthur Junior <arthurjr1906@gmail.com>
2023-10-25 20:32:21 -0300

    feat: Create ErrorMsg and HttpStatus code enum

---
+++ src/enums/ErrorMessages.ts
@@ -0,0 +1,6 @@
+enum ErrorMessages {
+  SHORT_USERNAME = 'Username must have 3 or more character!',
+  SHORT_PASSWORD = 'Password must have 6 or more character!',
+}
+
+export default ErrorMessages;
