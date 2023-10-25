fe0c5df16cf2f94b19587282a044acecfb3d7775
Arthur Junior <arthurjr1906@gmail.com>
2023-10-25 20:43:19 -0300

    feat: Add handleRepositoryError on repositoryClass

--- src/interfaces/UserRepository.ts
+++ src/interfaces/UserRepository.ts
@@ -1,7 +1,9 @@
+import CustomError from './CustomError';
 import User from './User';
 
 interface UserRepository {
   registerUser(userData: User): Promise<User>,
+  handleRepositoryError(err: unknown, customError: CustomError): void,
 }
 
 export default UserRepository;
--- src/repository/UserMongo.repository.ts
+++ src/repository/UserMongo.repository.ts
@@ -1,6 +1,9 @@
-import { Model } from 'mongoose';
+import { Model, mongo } from 'mongoose';
+
+import CustomError from '../interfaces/CustomError';
 
 import User from '../interfaces/User';
+import HttpStatusCode from '../enums/HttpStatusCode';
 import UserRepository from '../interfaces/UserRepository';
 import UserModel from '../model/User.model';
 
@@ -13,6 +16,14 @@ class UserMongoRepository implements UserRepository {
 
 	public async registerUser(userData: User): Promise<User> {
 		return this.model.create(userData);
+	}
+
+	public handleRepositoryError(err: unknown, customError: CustomError): void {
+		if (err instanceof mongo.MongoError) {
+			customError.setMessage('');
+			customError.setStatus(HttpStatusCode.CONFLICT);
+			console.log(customError);
+		}
 	}
 }
 
