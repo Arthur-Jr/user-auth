78256bb1b2eef954a0d4574a679561eec4198601
Arthur Junior <arthurjr1906@gmail.com>
2023-10-25 21:28:23 -0300

    feat: Create MongoConnection class

---
+++ src/model/MongoConnection.ts
@@ -0,0 +1,16 @@
+import DbConnection from '../interfaces/DbConnection';
+import mongoose from 'mongoose';
+
+class MongoConnection implements DbConnection {
+	private readonly uri: string;
+  
+	constructor(uri: string) {
+		this.uri = uri;
+	}
+
+	public connectDb(): void {
+		mongoose.connect(this.uri);
+	}
+}
+
+export default new MongoConnection(process.env.DB_URI || '');
