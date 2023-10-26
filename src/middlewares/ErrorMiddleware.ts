df3e3a2090c88dff3b31f80d6cda31674863f409
Arthur Junior <arthurjr1906@gmail.com>
2023-10-25 21:27:42 -0300

    fix: FIx error middleware

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import CustomError from '../interfaces/CustomError';
+import HttpStatusCode from '../enums/HttpStatusCode';
+import ErrorMessages from '../enums/ErrorMessages';
 
 class ErrorMiddleware {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public handleCustomError(err: CustomError, _req: Request, res: Response, _next: NextFunction) {
-		return res.status(err.getStatus()).json({ message: err.getMessage });
+		if (err.getStatus()) {
+			return res.status(err.getStatus()).json({ message: err.getMessage() });
+		}
+
+		return res.status(HttpStatusCode.INTERNAL).json({ message: ErrorMessages.INTERNAL_SERVER_ERROR });
 	}
 }
 
