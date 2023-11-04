2bc67019b1de2a725c8849cedec5880d1f00a38e
Arthur Junior <arthurjr1906@gmail.com>
2023-11-04 17:20:10 -0300

    feat: add login route

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+
 import RegisterController from '../controller/Register.controller';
+import LoginController from '../controller/Login.controller';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', (req, res, next) => RegisterController.registerNewUser(req, res, next));
+userRouter.post('/login', (req, res, next) => LoginController.login(req, res, next));
 
 export default userRouter;
