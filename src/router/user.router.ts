5eb3b0bd9b1dc670cf909fd46754ec4e63c5025f
Arthur Junior <arthurjr1906@gmail.com>
2023-11-07 22:43:29 -0300

    feat: add edit user on router

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import RegisterController from '../controller/Register.controller';
 import LoginController from '../controller/Login.controller';
+import UserManagerController from '../controller/UserManager.controller';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', (req, res, next) => RegisterController.registerNewUser(req, res, next));
 userRouter.post('/login', (req, res, next) => LoginController.login(req, res, next));
+userRouter.put('/', (req, res, next) => UserManagerController.editUser(req, res, next));
 
 export default userRouter;
