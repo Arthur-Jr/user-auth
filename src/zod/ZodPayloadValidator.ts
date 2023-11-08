cb422bf6ee1bd206bbffbd7750d9a7dcb549699a
Arthur Junior <arthurjr1906@gmail.com>
2023-11-07 21:09:56 -0300

    feat: add edit user schema on zod

--- src/zod/ZodPayloadValidator.ts
+++ src/zod/ZodPayloadValidator.ts
@@ -5,6 +5,7 @@ import ErrorMessages from '../enums/ErrorMessages';
 import HttpStatusCode from '../enums/HttpStatusCode';
 import PayloadValidator from '../interfaces/PayloadValidator';
 import UserPayload from '../interfaces/UserPayload';
+import EditUserPayload from '../interfaces/EditUserPayload';
 
 class ZodPayloadValidator implements PayloadValidator {
 	private readonly schema: ZodObject<ZodRawShape>;
@@ -14,7 +15,7 @@ class ZodPayloadValidator implements PayloadValidator {
 		this.schema = schema;
 	}
 
-	public validatePayload(userData: UserPayload): void{
+	public validatePayload(userData: UserPayload | EditUserPayload): void{
 		this.schema.parse(userData);
 	}
 
@@ -51,5 +52,15 @@ export const userLoginSchema = z.object({
 	password: z.string().min(6, { message: ErrorMessages.SHORT_PASSWORD })
 		.refine((value) => alphaNumRegex.test(value), { message: ErrorMessages.INVALID_PASSWORD }),
 }).partial({ email: true, username: true }).required({ password: true });
+
+export const editUserSchema = z.object({
+	username: z.string().min(3, { message: ErrorMessages.SHORT_USERNAME })
+		.refine((value) => alphaNumRegex.test(value), { message: ErrorMessages.INVALID_USERNAME }),
+	email: z.string().email(),
+	password: z.string().min(6, { message: ErrorMessages.SHORT_PASSWORD })
+		.refine((value) => alphaNumRegex.test(value), { message: ErrorMessages.INVALID_PASSWORD }),
+	newPassword: z.string().min(6, { message: ErrorMessages.SHORT_PASSWORD })
+		.refine((value) => alphaNumRegex.test(value), { message: ErrorMessages.INVALID_PASSWORD }),
+}).partial({ email: true, newPassword: true }).required({ username: true, password: true });
 
 export default ZodPayloadValidator;
