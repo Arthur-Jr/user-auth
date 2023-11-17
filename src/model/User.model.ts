99b2ea455568eddca6c6265b4348ab96bb13253e
Arthur Junior <arthurjr1906@gmail.com>
2023-11-17 15:20:03 -0300

    feat: update on email conditions on model

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -5,7 +5,7 @@ import User from '../interfaces/User';
 const userSchema = new Schema<User>(
 	{
 		username: { type: String, unique: true, required: true },
-		email: { type: String, unique: true },
+		email: { type: String, unique: true, sparse: true },
 		password: { type: String, required: true },
 		status: { type: Number },
 	},
