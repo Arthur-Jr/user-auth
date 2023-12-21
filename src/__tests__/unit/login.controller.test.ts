ba039f3fb0f83db3b8d0aae1afe4e07c62ee0fc2
Arthur Junior <arthurjr1906@gmail.com>
2023-12-21 16:41:43 -0300

    feat: Change login response

--- src/__tests__/unit/login.controller.test.ts
+++ src/__tests__/unit/login.controller.test.ts
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { describe, it, vi, expect, afterEach } from 'vitest';
 import { NextFunction, Request, Response } from 'express';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 
+import constants from '../../constants/constants';
 import LoginController from '../../controller/Login.controller';
-import LoginServiceImp from '../../service/Login.service';
 import HttpStatusCode from '../../enums/HttpStatusCode';
 import CustomErrorImp from '../../errors/CustomErrorImp';
+import LoginServiceImp from '../../service/Login.service';
 
 describe('Login controller unit tests:', () => {
 	const mockRequest = { body: {} };
 	const mockResponse = {
 		status: vi.fn().mockImplementation((_x: number) => mockResponse),
 		json: vi.fn().mockImplementation((x: unknown) => x),
+		cookie: vi.fn().mockImplementation((x: unknown, _token: unknown) => x),
 	} as Partial<Response>;
 	const mockNext = vi.fn();
 
@@ -19,7 +21,7 @@ describe('Login controller unit tests:', () => {
 		vi.clearAllMocks();
 	});
 
-	it('Login: should return status 200 and a auth-token', async () => {
+	it('Login: should return status 200 and set token on cookie', async () => {
 		const expectedResponseData =  { token: 'test' };
 		LoginServiceImp.login = vi.fn().mockImplementation(() => expectedResponseData);
 		await LoginController.login(mockRequest as Request, mockResponse as Response, mockNext as NextFunction);
@@ -27,7 +29,8 @@ describe('Login controller unit tests:', () => {
 		expect(LoginServiceImp.login).toBeCalledTimes(1);
 		expect(LoginServiceImp.login).toBeCalledWith(mockRequest.body);
 		expect(mockResponse.status).toBeCalledWith(HttpStatusCode.OK);
-		expect(mockResponse.json).toBeCalledWith(expectedResponseData);
+		expect(mockResponse.cookie).toBeCalledTimes(1);
+		expect(mockResponse.cookie).toBeCalledWith(constants.cookieTokenKeyName, expectedResponseData.token, constants.cookieDefaultSettings);
 		expect(mockNext).toBeCalledTimes(0);
 	});
 
