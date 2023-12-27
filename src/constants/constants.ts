aab8b28bdaa390ca23d0f28fe78c4f5815b2a031
Arthur Junior <arthurjr1906@gmail.com>
2023-12-27 11:29:01 -0300

    fix: fix cookie config

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
 	cookieTokenKeyName: 'userToken',
-	cookieDefaultSettings: { httpOnly: true, sameSite: 'None', secure: true, maxAge: (7 * 24 * 60 * 60 * 1000), path: '/' },
+	cookieDefaultSettings: { httpOnly: true, sameSite: 'Lax', secure: true, maxAge: (7 * 24 * 60 * 60 * 1000), path: '/' },
 };
 
 export default constants;
